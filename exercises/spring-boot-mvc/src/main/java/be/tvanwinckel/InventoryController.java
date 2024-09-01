81008eb5227b79928d0772677d18f4924fac69e8
Thomas Van Winckel <tvanwinckel@thomas-mbp14.localdomain>
2024-09-01 12:31:30 +0200

    Reset project and prepare for exercise params and headers.

--- exercises/spring-boot-mvc/src/main/java/be/tvanwinckel/InventoryController.java
+++ exercises/spring-boot-mvc/src/main/java/be/tvanwinckel/InventoryController.java
@@ -4,55 +4,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/inventory")
 public class InventoryController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(InventoryController.class);
+    private final List<String> items = List.of("Sword", "Shield", "Mana Potion");
+    private int gold = 0;
 
-    @PostMapping
-    public String postMapping(final Model model) {
-        model.addAttribute("message", "POST mapping");
-        return "inventoryView";
-    }
-
-    @RequestMapping(path = "/old-post", method = RequestMethod.POST)
-    public String oldPostMapping(final Model model) {
-        model.addAttribute("message", "old POST mapping");
-        return "inventoryView";
-    }
-
-    @GetMapping
+    @GetMapping(path = "/items")
     public String getInventory(final Model model) {
         LOGGER.info("Received a GET request on the inventoryMethod.");
-        model.addAttribute("message", "GET mapping");
+        final StringBuilder inventoryItemList = new StringBuilder();
+        for (final String item : items) {
+            inventoryItemList.append(" ").append(item);
+        }
+
+        model.addAttribute("message", inventoryItemList.toString());
         return "inventoryView";
     }
 
-    @PutMapping
-    public String putMapping(final Model model) {
-        model.addAttribute("message", "PUT mapping");
+    @GetMapping(path = "/gold")
+    public String getGoldFromInventory(final Model model) {
+        LOGGER.info("Received a GET request on the inventoryMethod.");
+        model.addAttribute("message", "Amount of gold: " + gold);
         return "inventoryView";
     }
-
-    @DeleteMapping
-    public String deleteMapping(final Model model) {
-        model.addAttribute("message", "DELETE mapping");
-        return "inventoryView";
-    }
-
-    @PatchMapping
-    public String patchMapping(final Model model) {
-        model.addAttribute("message", "PATCH mapping");
-        return "inventoryView";
-    }
-
 }
