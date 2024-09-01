4c1565f9a441833cb4ee3dcc61eafda607a9ebf8
Thomas Van Winckel <tvanwinckel@thomas-mbp14.localdomain>
2024-09-01 14:57:12 +0200

    Added a solution for thymeleaf forms exercise.

---
+++ exercises/spring-boot-mvc/src/main/java/be/tvanwinckel/CharacterController.java
@@ -0,0 +1,30 @@
+package be.tvanwinckel;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/characters")
+public class CharacterController {
+
+    private Character character;
+
+    public CharacterController() {
+        this.character = new Character("BigSwordGuy", "Warrior", "Horde", 55);
+    }
+
+    @GetMapping
+    public String personForm(final Model model) {
+        model.addAttribute("character", character);
+        return "characterForm";
+    }
+
+    @PostMapping
+    public String personSubmit(@ModelAttribute final Character character) {
+        return "characterView";
+    }
+}
