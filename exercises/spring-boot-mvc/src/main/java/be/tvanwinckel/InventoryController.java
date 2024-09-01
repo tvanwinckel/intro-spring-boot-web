8a08cc1785b041641ad209299c809a767d3ad42b
Thomas Van Winckel <tvanwinckel@thomas-mbp14.localdomain>
2024-09-01 11:52:23 +0200

    Setup exercise 2, interceptions.

---
+++ exercises/spring-boot-mvc/src/main/java/be/tvanwinckel/InventoryController.java
@@ -0,0 +1,22 @@
+package be.tvanwinckel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class InventoryController {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(InventoryController.class);
+
+    @GetMapping("/inventory")
+    public String getInventory(final Model model) {
+        LOGGER.info("Received a GET request on the inventoryMethod.");
+        model.addAttribute("items", "Empty");
+        return "inventoryView";
+    }
+}
