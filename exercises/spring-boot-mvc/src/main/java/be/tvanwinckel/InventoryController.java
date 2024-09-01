839cbb290704dcf832fdd7f453e85daa36b10560
Thomas Van Winckel <tvanwinckel@thomas-mbp14.localdomain>
2024-09-01 12:34:07 +0200

    Solution for exercise variables, params andheaders.

--- exercises/spring-boot-mvc/src/main/java/be/tvanwinckel/InventoryController.java
+++ exercises/spring-boot-mvc/src/main/java/be/tvanwinckel/InventoryController.java
@@ -5,8 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -14,18 +20,33 @@ import java.util.List;
 public class InventoryController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(InventoryController.class);
-    private final List<String> items = List.of("Sword", "Shield", "Mana Potion");
+    private final List<String> items = new ArrayList<>(Arrays.asList("Sword", "Shield", "Mana Potion"));
     private int gold = 0;
 
     @GetMapping(path = "/items")
-    public String getInventory(final Model model) {
+    public String getItemsFromInventory(final Model model) {
         LOGGER.info("Received a GET request on the inventoryMethod.");
-        final StringBuilder inventoryItemList = new StringBuilder();
-        for (final String item : items) {
-            inventoryItemList.append(" ").append(item);
-        }
+        model.addAttribute("message", inventoryItemsToString());
+        return "inventoryView";
+    }
 
-        model.addAttribute("message", inventoryItemList.toString());
+    @PostMapping(path = "/items")
+    public String addItemToInventory(@RequestParam(name = "item") final String item, final Model model) {
+        items.add(item);
+        model.addAttribute("message", inventoryItemsToString());
+        return "inventoryView";
+    }
+
+    @PostMapping(path = "/items2", params = "item")
+    public String addItemToInventory2(final String item, final Model model) {
+        items.add(item);
+        model.addAttribute("message", inventoryItemsToString());
+        return "inventoryView";
+    }
+
+    @GetMapping (path = "/items/{index}")
+    public String getItemFromInventory(@PathVariable(name = "index") final int itemIndex, final Model model) {
+        model.addAttribute("message", "The item at " + itemIndex + " is " + items.get(itemIndex));
         return "inventoryView";
     }
 
@@ -34,5 +55,27 @@ public class InventoryController {
         LOGGER.info("Received a GET request on the inventoryMethod.");
         model.addAttribute("message", "Amount of gold: " + gold);
         return "inventoryView";
+    }
+
+    @PostMapping(path = "/gold")
+    public String addGoldToInventory(@RequestParam(name = "amount", required = false, defaultValue = "0") final String amount,
+                                     @RequestHeader(name = "key") final String key,
+                                     final Model model) {
+        if(key.equals("secret")) {
+            gold = gold + Integer.parseInt(amount);
+            model.addAttribute("message", "Amount of gold: " + gold);
+        } else {
+            model.addAttribute("message", "Sorry, gold is locked");
+        }
+
+        return "inventoryView";
+    }
+
+    private String inventoryItemsToString() {
+        final StringBuilder inventoryItemList = new StringBuilder();
+        for (final String i : items) {
+            inventoryItemList.append(" ").append(i);
+        }
+        return inventoryItemList.toString();
     }
 }
